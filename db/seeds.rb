54391a8905f8c78779a9e9d5f5d77c2245e69397
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-11-09 17:34:50 +0300

    added data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ puts "🌱 Seeding spices..."
 
 item1=Product.create(
     name: "Budweiser RootBeer",
-    price: "1,900",
+    price: "1900",
     category_name: "vodka",
     description: " A delicious ready to drink margarita cocktail with fresh citrus aromas and juicy lime and unique orange flavors that finish with a slightly spicy tequila taste",
     image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4LkDs5c80z0r2wsRR3A5yUsO4drFSVOynlA&usqp=CAU",
@@ -16,7 +16,7 @@ item1=Product.create(
 
  item2=Product.create(
     name: "vino wine",
-    price: "6,999",
+    price: "6999",
     category_name: "wine",
     description: " A delicious ready to drink margarita cocktail with fresh citrus aromas and juicy lime and unique orange flavors that finish with a slightly spicy tequila taste",
     image_url: "https://digitalcontent.api.tesco.com/v2/media/ghs/79526dda-2572-4f83-bea0-12fdbac6e7c0/77f72a7c-3626-414a-b7a8-68381849b282_1602607098.jpeg?h=540&w=540",
@@ -26,7 +26,7 @@ item1=Product.create(
 
   item3=Product.create(
     name: "Gilbeys Vodka",
-    price: "2,499",
+    price: "2499",
     category_name: "vodka",
     description: " A delicious ready to drink margarita cocktail with fresh citrus aromas and juicy lime and unique orange flavors that finish with a slightly spicy tequila taste. A great cocktail to enjoy with friends for easy and convenient entertaining. Enjoy!",
     image_url: "https://products3.imgix.drizly.com/ci-gilbeys-vodka-f22ba07845c24314.jpeg?auto=format%2Ccompress&ch=Width%2CDPR&fm=jpg&q=20",
@@ -36,7 +36,7 @@ item1=Product.create(
 
   item4=Product.create(
     name: "Heineken Beer",
-    price: "1,500",
+    price: "1500",
     description: "Smooth, nicely blended bitterness, clean finish. Wherever you go in the world, it’s always refreshing to see something you recognize. That green bottle, the red star, the smiling ‘e’… like an instant welcome from an old friend. Cold, fresh, high quality Heineken. Enjoyed near and far since 1873.",
     category_name: "Beer",
     image_url: "https://products2.imgix.drizly.com/ci-heineken-lager-71197a28a91c1b6c.jpeg?auto=format%2Ccompress&ch=Width%2CDPR&fm=jpg&q=20",
@@ -47,7 +47,7 @@ item1=Product.create(
 
   item5=Product.create(
     name: "Budweiser RootBeer",
-    price: "1,900",
+    price: "1900",
     category_name: "Beer",
     description: "Root beer is a sweet North American soft drink traditionally made using the root bark of the sassafras tree Sassafras albidum or the vine of Smilax ornata (known as sarsaparilla, also used to make a soft drink, sarsaparilla) as the primary flavor. Root beer is typically but not exclusively non-alcoholic, caffeine-free, sweet, and carbonated. ",
     image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4LkDs5c80z0r2wsRR3A5yUsO4drFSVOynlA&usqp=CAU",
@@ -58,7 +58,7 @@ item1=Product.create(
 
   item6=Product.create(
     name: "Eruption 6 Pack",
-    price: "2,499",
+    price: "2499",
     category_name: "Beer",
     description: " A delicious ready to drink margarita cocktail with fresh citrus aromas and juicy lime and unique orange flavors that finish with a slightly spicy tequila taste",
     image_url: "https://cdn.shopify.com/s/files/1/2572/9722/products/The_Experimental_Mixed_Six_Pack_XXV_808x.png?v=1570521143",
@@ -68,7 +68,7 @@ item1=Product.create(
 
   item7=Product.create(
     name: "Tall Dark Rum",
-    price: "6,999",
+    price: "6999",
     category_name: "Beer",
     description: "Captain Morgan Dark Rum is made with a secret blend of fine Caribbean rums and exotic spices to make our darkest flavour rum. Captain Morgan Dark Rum bursts with toffee, rich caramel, sweet dark brown sugar and ripe banana flavours.",
     image_url: "https://wineandmore.s3.amazonaws.com/img/2021/01/23185022/Captain-Morgan-Dark-Rum-1L-200x200.png",
@@ -77,7 +77,7 @@ item1=Product.create(
   )
   item8=Product.create(
     name: "Soweto Ruins Beer",
-    price: "1,999",
+    price: "1999",
     category_name: "Beer",
     description: " A delicious ready to drink margarita cocktail with fresh citrus aromas and juicy lime and unique orange flavors that finish with a slightly spicy tequila taste",
     image_url: "https://i.ytimg.com/vi/VO-8l5yCnrI/mqdefault.jpg",
@@ -88,7 +88,7 @@ item1=Product.create(
 
   item9=Product.create(
     name: "Tall Dark Rum",
-    price: "6,999",
+    price: "6999",
     category_name: "Beer",
     description: "Captain Morgan Dark Rum is made with a secret blend of fine Caribbean rums and exotic spices to make our darkest flavour rum. Captain Morgan Dark Rum bursts with toffee, rich caramel, sweet dark brown sugar and ripe banana flavours.",
     image_url: "https://wineandmore.s3.amazonaws.com/img/2021/01/23185022/Captain-Morgan-Dark-Rum-1L-200x200.png",
@@ -99,7 +99,7 @@ item1=Product.create(
 
   item10=Product.create(
     name: "Budweiser RootBeer",
-    price: "1,900",
+    price: "1999",
     category_name: "Beer",
     description: "Root beer is a sweet North American soft drink traditionally made using the root bark of the sassafras tree Sassafras albidum or the vine of Smilax ornata (known as sarsaparilla, also used to make a soft drink, sarsaparilla) as the primary flavor. Root beer is typically but not exclusively non-alcoholic, caffeine-free, sweet, and carbonated. ",
      image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4LkDs5c80z0r2wsRR3A5yUsO4drFSVOynlA&usqp=CAU",
@@ -110,7 +110,7 @@ item1=Product.create(
 
   item11=Product.create(
     name: "Gilbeys Vodka",
-    price: "2,499",
+    price: "2499",
     category_name: "wine",
     description: "A delicious ready to drink margarita cocktail with fresh citrus aromas and juicy lime and unique orange flavors that finish with a slightly spicy tequila taste. A great cocktail to enjoy with friends for easy and convenient entertaining. Enjoy!",
     image_url: "https://products3.imgix.drizly.com/ci-gilbeys-vodka-f22ba07845c24314.jpeg?auto=format%2Ccompress&ch=Width%2CDPR&fm=jpg&q=20",
