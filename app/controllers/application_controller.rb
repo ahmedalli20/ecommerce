5b4689dbd25aa000cbd4a48b5428ccfb8ae07eeb
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2022-11-09 11:03:55 +0300

    added the get and post methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,29 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-    get '/' do
-      '<h2>Hello <em>World</em>!</h2>'
-    end
-  
+  get '/products' do
+    products = Product.all
+    
+    products.to_json
+  end
+
+
+  get '/products/:id' do
+    product = Product.find(params[:id])
+    product.to_json
+  end
+
+  post '/addproduct' do
+    product= Product.create(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      category_name: params[:category_name],
+      quantity: params[:quantity],
+      image_url: params[:image_URL]
+    )
+    product.to_json
+  end
+   
+
   end
